e887cbed260d270bcbb32fe0784878662727d285
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-23 01:48:37 +0000

    all buttons added

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
 import kivy
 
+Config.set('graphics', 'resizable', 1)
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '200')
 
 class MyRoot(BoxLayout):
+
+    Config.set('graphics', 'resizable', 1)
+    # Config.set('graphics', 'width', '400')
+    # Config.set('graphics', 'height', '200')
 
     def __init__(self):
         super(MyRoot, self).__init__()
