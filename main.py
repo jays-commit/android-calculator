a28d86ffc3526983ad570e1f7fbbf5a8f61d4375
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-23 00:57:09 +0000

    Kv file working and first 3 buttons made

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+import kivy
+
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def calc_symbol(self, symbol):
+        self.calc_field.text += symbol
+
+
+class AndroidCalculator(App):
+
+    def build(self):
+        return MyRoot()
+
+
+androidcalculator = AndroidCalculator()
+androidcalculator.run()
